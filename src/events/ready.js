c89fc3c8f55050891a191d3649c83b98555016f5
NodoY5 <71686820+NodoY5@users.noreply.github.com>
2021-05-22 20:38:05 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,16 +1,18 @@
-
-const Client = require('../classes/Unicron');
-const BaseEvent = require('../classes/BaseEvent');
+const Client = require("../classes/Unicron");
+const BaseEvent = require("../classes/BaseEvent");
 module.exports = class extends BaseEvent {
-    constructor() {
-        super('ready');
-    }
-    /**
-     * 
-     * @param {Client} client 
-     */
-    async run(client) {
-        client.forceSweep(70);
-        client.startSweepInterval();
-    }
-}
+  constructor() {
+    super("ready");
+  }
+  /*
+   *
+   * @param {Client} client
+   */
+  async run(client) {
+    client.forceSweep(70);
+    client.startSweepInterval();
+client.user.setActivity(`Under Maintenance, Join Support Server for more info! | !support`, { type : "PLAYING" })
+  }
+};
+
+//LOL
