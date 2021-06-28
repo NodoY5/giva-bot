af17a2d78516d5241b0c33511d58af96fddb2fdd
NodoY5 <aaravmehta59@outlook.com>
2021-06-28 21:26:53 +0530

    LOL
    We did it....
    
    Co-authored-by: SquadXX <aawasa@gmail.com>

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,7 +11,7 @@ module.exports = class extends BaseEvent {
   async run(client) {
     client.forceSweep(70);
     client.startSweepInterval();
-client.user.setActivity(`Under Maintenance, Join Support Server for more info | !support `, { type : "WATCHING" })
+client.user.setActivity(`My New Website!! | https://giva-bot.vercel.app/ `, { type : "WATCHING" })
   }
 };
 
