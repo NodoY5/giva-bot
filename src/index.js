d1141a087dccfcaa502c4c972131079c8b2bbcf9
NodoY5 <aaravmehta59@outlook.com>
2021-07-07 22:04:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,14 +13,6 @@ const Manager = new ShardingManager('./src/Unicron.js', {
     shardArgs: ['--shard'],
     respawn: true,
 });
-
-var http = require('http');  
-http.createServer(function (req, res) {   
-  res.write("I'm alive");   
-  res.end(); 
-}).listen(8080);
-client.config = config;
-client.queue = new Map();
   
 //Shard Handler
   
